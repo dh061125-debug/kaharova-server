8874a4ab16462a447785a27decd7aa9161ffde96
dh061125 <dh061125@gmail.com>
2026-09-22 22:59:06 +0900

    fix websocket send

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import os
 import logging
 import websockets
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
 
 # --- State ---
